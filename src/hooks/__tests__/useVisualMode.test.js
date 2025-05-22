a90deda7a186da6ff46b2ee83b166a840f5e7508
manavpanchotiya <manavpanchotiya@gmail.com>
2025-05-21 21:34:11 -0500

    added transition test

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -1,9 +1,13 @@
 import { renderHook } from "@testing-library/react";
 import useVisualMode from "../useVisualMode";
+import { act } from "react";
 
 const FIRST = "FIRST";
+const SECOND = "SECOND";
 
 test("useVisualMode initializes with the initial mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
-  expect(result.current.mode).toBe(FIRST);
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
 });
