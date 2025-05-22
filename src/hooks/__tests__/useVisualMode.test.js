d0dcb4649ec1848dae59f0b28341c3ca0bb93261
manavpanchotiya <manavpanchotiya@gmail.com>
2025-05-21 21:52:17 -0500

    added Back transition test

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -4,10 +4,21 @@ import { act } from "react";
 
 const FIRST = "FIRST";
 const SECOND = "SECOND";
+const THIRD = "THIRD";
 
 test("useVisualMode initializes with the initial mode", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
 
   act(() => result.current.transition(SECOND));
   expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
+
 });
